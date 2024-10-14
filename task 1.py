e2de0848c0060a1ad7fe10974a1c543c51745fc4
Audmiral <joshua.gulbin@gmail.com>
2024-10-14 08:57:15 +0800

    una

---
+++ task 1.py
@@ -0,0 +1,14 @@
+def check_grade(grade):
+    if grade >= 90 and grade <= 100:
+        return "Ecellent! You did a great job!"
+    elif grade >= 80 and grade <= 89:
+        return "Good job it up!"
+    elif grade >= 70 and grade <= 79:
+        return "You passed but there's  room for improvement."
+    elif grade >= 70:
+        return "You failled. Better luck next time."
+    else:
+        return "invalid"
+grade = int(input("enter your grade"))
+print(check_grade(grade))
+        
